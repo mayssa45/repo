9d55bda0ff54c1993ac108b99c4661e208752968
ameni <amanibenjeddou.abj@gmail.com>
2023-03-11 10:10:40 +0100

    main chnages

--- src/main/java/com/example/kaddem/KaddemApplication.java
+++ src/main/java/com/example/kaddem/KaddemApplication.java
@@ -2,7 +2,7 @@ package com.example.kaddem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//this is the main function
 @SpringBootApplication
 public class KaddemApplication {
 
